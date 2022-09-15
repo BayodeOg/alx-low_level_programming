ff9f08214a469e20f08729f3bead5d3a04967e82
BayodeOg <bayodeog@gmail.com>
2022-09-15 22:10:59 +0100

    0. isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing proototypes for all functions
+ * used in the 0x03-more_functions_nested_loops directory.
+ * void protoypes (void)
+ * int protoypes (int)
+ */
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif
