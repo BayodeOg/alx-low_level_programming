402750936cf9bbfce458a6e4cc013553a719a6a3
BayodeOg <bayodeog@gmail.com>
2022-09-22 18:58:14 +0100

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,7 +19,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
